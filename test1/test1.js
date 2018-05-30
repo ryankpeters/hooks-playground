01d701f5b5ef62f1bd7adc0bcb382721be13f5fa
Ryan Peters <ryan.peters@turner.com>
2018-05-30 19:30:44 -0400

    creating two tests

---
+++ test1/test1.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+  // CONTRIBUTED SOLUTION
+  // let arr = [...str];
+  // let newArr = arr.map((_, i, arr) => {
+  //     return arr[arr.length - 1 - i];
+  //   });
+  // return newArr.join("");
+  // CONTRIBUTED SOLUTION
+  // BY Ankita Patil
+  // return str.split("").reduce((reversedString, character) => character + reversedString, "");
+}
---
+++ test2/test2.js
@@ -0,0 +1,13 @@
+function isPalindrome(str) {
+  // CONTRIBUTED SOLUTION
+  //     let isPali = true;
+  //     let caseStr = str.toLowerCase()
+  //     let arr = caseStr.split('')
+  //     for(let i = 0; i < arr.length; i++){
+  //         if(arr[i] !== arr[arr.length -1 -i]){
+  //             isPali = false;
+  //             break;
+  //         }
+  //     }
+  //     return isPali
+}
