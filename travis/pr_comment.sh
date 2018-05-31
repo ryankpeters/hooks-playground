05b31cce98dc919fa2f041000f09fa55b3fdb855
Ryan Peters <ryan.peters@turner.com>
2018-05-30 23:45:22 -0400

    syntax blah

--- travis/pr_comment.sh
+++ travis/pr_comment.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
 -d "{\"body\": \"Automagical comment\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
