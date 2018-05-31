f005fd6ffce5fb2394a50395846870b5bce0d0ce
Ryan Peters <ryan.peters@turner.com>
2018-05-31 00:41:18 -0400

    hard coded token for test

--- travis/pr_comment.sh
+++ travis/pr_comment.sh
@@ -1,5 +1,11 @@
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#curl -H "Authorization: token $GITHUB_TOKEN" -X POST \
+#-d "{\"body\": \"Automagical comment\"}" \
+#"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+#fi
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+curl -H "Authorization: token 0ec99de1e53dc70453ad2310370a8915dd4609ee" -X POST \
 -d "{\"body\": \"Automagical comment\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
